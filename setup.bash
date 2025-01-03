b863d35606c6965d690057077e3eeae864728d80
kenta-kawamata <university.kawamata@gmail.com>
2025-01-03 23:33:22 +0900

    update

--- setup.bash
+++ setup.bash
@@ -1,11 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
 #If your host OS env is linux : linux
 #If your host OS env is linux : wsl
 HOST_OS_ENV="linux"
 
-if [$HOST_OS_ENV = "linux"]; then
-    compose -f compose.yml up -d
-elif [$HOST_OS_ENV = "wsl"]; then
-    compose -f compose.wsl.yml up -d
+if [${HOST_OS_ENV} = "linux"]; then
+  docker compose -f compose.yml up -d
+elif [${HOST_OS_ENV} = "wsl"]; then
+  docker compose -f compose.wsl.yml up -d
+else
+  echo "${HOST_OS_ENV} is not defined in the setup.bash script"
 fi
